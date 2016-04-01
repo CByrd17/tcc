d31f858918d8235bd2098d139f1bdced04c4c8a0
CByrd17 <cbyrd17@yahoo.com>
2016-04-01 15:24:04 -0400

    Tree structure more or less built. Now for the algorithm...

--- tcc/src/tcc/srm144/div2/PowerOutage.java
+++ tcc/src/tcc/srm144/div2/PowerOutage.java
@@ -3,7 +3,10 @@
  */
 package tcc.srm144.div2;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import tcc.srm144.div2.PowerOutage.Node;
 
 /**
  * @author cbyrd17
@@ -16,11 +19,76 @@ public class PowerOutage {
 		private int distanceFromParent;
 		private Node<T> parent;
 		private List<Node<T>> children;
-		
-		public Node (T nameToUse, Node<T> parentToUse) {
+
+		Node(final T nameToUse, final Node<T> parentToUse,
+				final int distance) {
 			name = nameToUse;
 			parent = parentToUse;
-			
+			distanceFromParent = distance;
+			children = new ArrayList<Node<T>>();
+		}
+
+		public Node<T> find(T nameToFind) {
+			Node<T> found = null;
+			if (name.equals(nameToFind)) {
+				found = this;
+			} else {
+				for (Node<T> child : children) {
+					Node<T> foundOne = child.find(nameToFind);
+					if (foundOne != null) {
+						found = foundOne;
+						break;
+					}
+				}
+			}
+			return found;
+		}
+
+		public void addChild(Node<T> nodeToAdd) {
+			children.add(nodeToAdd);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#toString()
+		 */
+		public String toString() {
+			return this.toString(0);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#toString()
+		 */
+		public String toString(int depth) {
+			StringBuffer sb = new StringBuffer(name.toString());
+
+			boolean moreChildren = true;
+
+			for (int i = 0; i < children.size(); i++) {
+				Node<T> child = children.get(i);
+				sb.append("--- " + child.distanceFromParent + " --->"
+						+ child.toString(depth + 1));
+				if (i == children.size() - 1) {
+					moreChildren = false;
+				}
+				if (moreChildren) {
+					sb.append(System.lineSeparator());
+					for (int j = 0; j < depth; j++) {
+						sb.append("           ");
+					}
+					sb.append('|');
+					sb.append(System.lineSeparator() + ' ');
+					for (int j = 0; j < depth; j++) {
+						sb.append("           ");
+					}
+				}
+			}
+
+			return sb.toString();
+
 		}
 	}
 
@@ -53,9 +121,15 @@ public class PowerOutage {
 	 */
 	public final int estimateTimeOut(final int[] fromJunction,
 			final int[] toJunction, final int[] ductLength) {
+		Node<Integer> root = new Node<Integer>(0, null, 0);
 		for (int current = 0; current < fromJunction.length; current++) {
-			
+			Node<Integer> parent = root.find(fromJunction[current]);
+			Node<Integer> child = new Node<Integer>(toJunction[current],
+					parent, ductLength[current]);
+			parent.addChild(child);
 		}
+
+		System.out.println(root.toString(0));
 
 		return 10;
 	}
--- tcc/src/test/srm144/div2/TestPowerOutage.java
+++ tcc/src/test/srm144/div2/TestPowerOutage.java
@@ -21,8 +21,9 @@ public class TestPowerOutage {
 		PowerOutage powerOutage = new PowerOutage();
 
 		final int[] fromJunctionSeed = { 0 };
-		final int[] toJunctionSeed = { 1 };
-		final int[] ductLengthSeed = { 10 };
+		final int[] ductLengthSeed =  { 10 };
+		final int[] toJunctionSeed =   { 1 };
+
 
 		final int expected = 10;
 
@@ -36,11 +37,63 @@ public class TestPowerOutage {
 	public final void testEstimateTimeOut1() {
 		PowerOutage powerOutage = new PowerOutage();
 
-		final int[] fromJunctionSeed = { 0, 1, 0 };
-		final int[] toJunctionSeed = { 1, 2, 3 };
-		final int[] ductLengthSeed = { 10, 10, 10 };
+		final int[] fromJunctionSeed = { 0,  1,  0 };
+		final int[] ductLengthSeed =  { 10, 10, 10 };
+		final int[] toJunctionSeed =   { 1,  2,  3 };
+
 
 		final int expected = 40;
+
+		final int result = powerOutage.estimateTimeOut(fromJunctionSeed,
+				toJunctionSeed, ductLengthSeed);
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public final void testEstimateTimeOut2() {
+		PowerOutage powerOutage = new PowerOutage();
+
+		final int[] fromJunctionSeed = { 0,  0,   0,  1, 4 };
+		final int[] ductLengthSeed = {  10, 10, 100, 10, 5 };
+		final int[] toJunctionSeed = {   1,  3,   4,  2, 5 };
+
+
+		final int expected = 165;
+
+		final int result = powerOutage.estimateTimeOut(fromJunctionSeed,
+				toJunctionSeed, ductLengthSeed);
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public final void testEstimateTimeOut3() {
+		PowerOutage powerOutage = new PowerOutage();
+
+		final int[] fromJunctionSeed = {  0,  0,   0,  1, 4, 4, 6,   7, 7,  7, 20 };
+		final int[] ductLengthSeed =   { 10, 10, 100, 10, 5, 1, 1, 100, 1,  1,	5 };
+		final int[] toJunctionSeed =   {  1,  3,   4,  2, 5, 6, 7,  20, 9, 10, 31 };
+
+
+		final int expected = 281;
+
+		final int result = powerOutage.estimateTimeOut(fromJunctionSeed,
+				toJunctionSeed, ductLengthSeed);
+
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public final void testEstimateTimeOut4() {
+		PowerOutage powerOutage = new PowerOutage();
+
+		final int[] fromJunctionSeed = { 0,0,0,0,0};
+		final int[] ductLengthSeed =   {100,200,300,400,500};
+		final int[] toJunctionSeed =   {1,2,3,4,5};
+
+
+		final int expected = 2500;
 
 		final int result = powerOutage.estimateTimeOut(fromJunctionSeed,
 				toJunctionSeed, ductLengthSeed);
